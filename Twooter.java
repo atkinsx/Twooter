e72ec8011e33e7b9d9e833a592bb52233268f9cc
Atkins <atkinsx@egb070000051.lancs.local>
2019-05-01 11:35:21 +0100

    Attempted basic connection and creating a user account

--- Twooter.java
+++ Twooter.java
@@ -1,7 +1,25 @@
+import twooter.*;
+import javax.swing.*;
+
 public class Twooter
 {
     public static void main(String[] args)
     {
+        TwooterClient test = new TwooterClient();
+        String idk;
+        System.out.println("Hello world");
+
+        test.enableLiveFeed();
         
+
+        try
+        {
+            idk = test.registerName("helpmewhatisgoingon");
+            System.out.println(idk);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
     }
 }
